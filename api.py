111a92476cb4e15a51b66a25da03195c7ec01405
Mohammad Danish <danish.sab05@gmail.com>
2026-03-03 03:33:13 +0530

    Intial commit

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, Path
 import json
 
 app = FastAPI()
@@ -22,3 +22,11 @@ def view():
     data = load_data()
 
     return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id:str = Path(..., description ='ID of the patient in the DB', example ='POO1')):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    return {'error':'patient not Found'}
