176df8063b28a35c4739139e0557e2289f572a0d
Mohammad Danish <danish.sab05@gmail.com>
2026-03-05 04:34:48 +0530

    first commit

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException 
 import json
 
 app = FastAPI()
@@ -29,4 +29,6 @@ def view_patient(patient_id:str = Path(..., description ='ID of the patient in t
 
     if patient_id in data:
         return data[patient_id]
-    return {'error':'patient not Found'}
+    # return {'error':'patient not Found'}
+    raise HTTPException(status_code=404, detail='Patient not Found')
+
